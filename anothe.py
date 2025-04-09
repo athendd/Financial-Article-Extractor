f398c2a41abf6f7e0f60199aded0a8595ca464ca
athendd <141829395+athendd@users.noreply.github.com>
2025-04-09 13:50:42 -0400

    Update anothe.py

--- anothe.py
+++ anothe.py
@@ -1,237 +1,176 @@
 import requests
-import TextSummarizer
 from bs4 import BeautifulSoup
-from datetime import datetime
-import re
+import TextSummarizer
 import pandas as pd
-from collections import OrderedDict
+import numpy as np
 import spacy
 import mysql.connector
 from mysql.connector import Error
-import os 
-import numpy as np
+import os
 
-def connect_to_server(host_name, user_name, user_password):
-    connection = None
+nlp = spacy.load("en_core_web_sm")
+
+def connect_to_server(host, user, password):
     try:
-        connection = mysql.connector.connect(host = host_name, user = user_name, password = user_password)
-        
+        return mysql.connector.connect(host=host, user=user, password=password)
     except Error as e:
-        print(f"The error {e} occurred")
-    return connection
-        
+        print(f"Connection error: {e}")
+        return None
+
 def connect_database(host, user, password, db_name):
-    database_connection = None
     try:
-        database_connection = mysql.connector.connect(host = host, user = user, password = password, database = db_name)
+        return mysql.connector.connect(host=host, user=user, password=password, database=db_name)
     except Error as e:
-        print(f"Error {e} occurred while connecting to the databse")
-    return database_connection
+        print(f"DB connection error: {e}")
+        return None
 
 def table_exists(cursor, table_name):
     cursor.execute(f"SHOW TABLES LIKE '{table_name}'")
-    result = cursor.fetchone()
-    return result is not None
-
+    return cursor.fetchone() is not None
 
 def find_author(soup):
-    div = soup.find('div', class_ = 'caas-attr-item-author')
-    if div:
-        author = div.find('span', class_ = 'caas-author-byline-collapse')
-        if author:
-            return author.text.strip()
-        
-        else:
-            return None
-    else:
-        return None
-    
+    tag = soup.select_one('div.caas-attr-item-author span.caas-author-byline-collapse')
+    return tag.text.strip() if tag else None
+
 def find_title(soup):
-    title = soup.find('div', class_ = 'caas-title-wrapper')
-    if title:
-        return title.text.strip()
-    else:
-        return None
+    tag = soup.select_one('div.caas-title-wrapper')
+    return tag.text.strip() if tag else None
 
 def find_time(soup):
-    div = soup.find('div', class_ = 'caas-attr-time-style')
-    if div:
-        time = div.find('time')
-        if time:
-            return time.text.strip()
-        else:
-            return None
-    else:
-        return None
-    
+    tag = soup.select_one('div.caas-attr-time-style time')
+    return tag.text.strip() if tag else None
+
 def find_body(soup):
-    article_body = soup.find('div', class_ = 'caas-body')
-    if article_body:
-        summary = TextSummarizer.perform_textrank(article_body.text.strip())
-        if summary:
-            return article_body.text.strip(), summary
-        else:
-            return article_body.text.strip(), None
-    else:
-        return None, None
-    
-def find_stocks(soup):
-    fin_ticker_elements = soup.find_all('fin-ticker')
-    if fin_ticker_elements:
-        stocks = {}
-        for stock in fin_ticker_elements:
-            symbol = stock['symbol']
-            if '^' in symbol:
-                symbol = symbol.replace('^', '%5E')
-            link = f'https://finance.yahoo.com/quote/{symbol}'
-            third_response = requests.get(link)
-            if third_response:
-                third_soup = BeautifulSoup(third_response.content, 'html.parser')
-                stocks[symbol] = find_stock(third_soup)
-    else:
-        return None
-    return stocks
-            
-def find_stock(third_soup):
-    stock_dic = {}
-    Company_name = third_soup.find('h1', class_ = 'svelte-3a2v0c').text.split('(')[0]
-    stock_dic['name'] = Company_name
-    Current_Price = third_soup.find('fin-streamer', class_ = 'livePrice svelte-mgkamr').text
-    stock_dic['price'] = Current_Price
-    
-    containers = third_soup.find('div', {'data-testid': 'quote-statistics'})
-    labels = containers.find_all('span', class_ = 'label svelte-tx3nkj')
-    values = containers.find_all('span', class_ = 'value svelte-tx3nkj')
-    for i in range(len(values)):
-        if values[i].text == '--':
-            value = None
-        value = values[i].text
-        label = labels[i].text
-        
-        stock_dic[label] = value
-    
-    return stock_dic
-
-
-def extract_sentences(text, company):
-    nlp = spacy.load("en_core_web_sm")
-    doc = nlp(text)
-    sentence = ''
-    for sent in doc.sents:
-        if company in sent.text:
-            sentence = sent.text
-    return sentence
+    body_tag = soup.select_one('div.caas-body')
+    if body_tag:
+        full_text = body_tag.text.strip()
+        summary = TextSummarizer.perform_textrank(full_text)
+        return full_text, summary
+    return None, None
 
 def contains_integer(s):
-    for c in s:
-        if c.isdigit():
-            return True
-    return False
+    return any(char.isdigit() for char in s)
 
+def extract_sentences(text, company):
+    doc = nlp(text)
+    for sent in doc.sents:
+        if company in sent.text:
+            return sent.text
+    return None
 
-url = "https://finance.yahoo.com/"
-response = requests.get(url)
-soup = BeautifulSoup(
-        response.content, 
-        'html.parser')
-article_urls = soup.find_all('a', href=lambda href: href and href.startswith('https://finance.yahoo.com/news/'))
-article_urls = set(article_urls)
-article_dic = {}
-all_stocks = {}
-article_dic['link'] = []
-article_dic['author'] = []
-article_dic['title'] = []
-article_dic['time'] = []
-article_dic['sum_body'] = []
-article_dic['stocks'] = []
-counter = 0
-for url in article_urls:
-    if url['href'] == 'https://finance.yahoo.com/news/':
-        continue
-    if counter == 3:
-        break
-    link = url['href']
-    article_dic['link'].append(url['href'])
-    second_response = requests.get(link)
-    second_soup = BeautifulSoup(second_response.content, 'html.parser')
+def find_stock_info(symbol):
+    stock_url = f'https://finance.yahoo.com/quote/{symbol}'
+    response = requests.get(stock_url)
+    if not response.ok:
+        return None
 
-    article_dic['author'].append(find_author(second_soup))
-    article_dic['title'].append(find_title(second_soup))
-    article_dic['time'].append(find_time(second_soup))    
-    body, sum_body = find_body(second_soup)
-    article_dic['sum_body'].append(sum_body)
-    stock_info = find_stocks(second_soup)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    stock_data = {}
+
+    try:
+        stock_data['name'] = soup.select_one('h1.svelte-3a2v0c').text.split('(')[0].strip()
+        stock_data['price'] = soup.select_one('fin-streamer.livePrice').text
+    except Exception:
+        return None
+
+    stats_container = soup.select_one('div[data-testid="quote-statistics"]')
+    if stats_container:
+        labels = stats_container.select('span.label')
+        values = stats_container.select('span.value')
+        for label, value in zip(labels, values):
+            stock_data[label.text] = value.text if value.text != '--' else None
+
+    return stock_data
+
+def find_stocks(soup):
+    stocks = {}
+    for tag in soup.find_all('fin-ticker'):
+        symbol = tag.get('symbol', '')
+        symbol_url = symbol.replace('^', '%5E')
+        stock_info = find_stock_info(symbol_url)
+        if stock_info:
+            stocks[symbol] = stock_info
+    return stocks if stocks else None
+
+def scrape_yahoo_articles(limit=3):
+    base_url = "https://finance.yahoo.com/"
+    soup = BeautifulSoup(requests.get(base_url).content, 'html.parser')
+    links = set(a['href'] for a in soup.find_all('a', href=True) if a['href'].startswith('https://finance.yahoo.com/news/') and a['href'] != 'https://finance.yahoo.com/news/')
+
+    articles = {
+        'link': [], 'author': [], 'title': [], 'time': [],
+        'sum_body': [], 'stocks': []
+    }
+    all_stocks = {}
     
-    if stock_info:
-        stocks = list(stock_info.keys())
-        total_stocks = [stock.replace('%5E', '') for stock in stocks]
-        result = ', '.join(total_stocks)
-        article_dic['stocks'].append(result)
-    else:
-        article_dic['stocks'].append(None)
-    if stock_info and body:
-        for stock in stock_info.keys():
-            if stock_info[stock]['name']:
-                if contains_integer(stock_info[stock]['name']):
-                   stock_name = stock_info[stock]['name']
-                else: 
-                    stock_name = stock_info[stock]['name'].split(' ')[0]
-                stock_info[stock]['sentence'] = stock_sentences = extract_sentences(body, stock_name)
-        all_stocks.update(stock_info)
-    
-    counter += 1
-    
+    for i, link in enumerate(links):
+        if i >= limit:
+            break
 
-stock_df = pd.DataFrame(all_stocks)
-stock_df.replace('--', np.nan, inplace=True)
-one = stock_df.transpose()
+        article_soup = BeautifulSoup(requests.get(link).content, 'html.parser')
+        body, summary = find_body(article_soup)
+        stock_info = find_stocks(article_soup)
 
-if os.path.exists(r'C:\Users\thynnea\Downloads\stocks.csv'):
-    one.to_csv(r'C:\Users\thynnea\Downloads\stocks.csv', mode= 'a', index = False, header = False)
-else:
-    one.to_csv(r'C:\Users\thynnea\Downloads\stocks.csv', index = False)
+        articles['link'].append(link)
+        articles['author'].append(find_author(article_soup))
+        articles['title'].append(find_title(article_soup))
+        articles['time'].append(find_time(article_soup))
+        articles['sum_body'].append(summary)
+        articles['stocks'].append(', '.join([s.replace('%5E', '') for s in stock_info]) if stock_info else None)
 
-df = pd.DataFrame(article_dic)
-if os.path.exists(r'C:\Users\thynnea\Downloads\articles.csv'):
-    df.to_csv(r'C:\Users\thynnea\Downloads\articles.csv', mode = 'a', index = False, header = False)
-else:
-    df.to_csv(r'C:\Users\thynnea\Downloads\articles.csv', index=False, header = True)
-    
-df['time'] = pd.to_datetime(df['time'], format = '%a, %b %d, %Y, %I:%M %p')
+        if stock_info and body:
+            for symbol, data in stock_info.items():
+                company = data['name']
+                name_for_search = company if contains_integer(company) else company.split(' ')[0]
+                data['sentence'] = extract_sentences(body, name_for_search)
+            all_stocks.update(stock_info)
 
-df.drop('sum_body', axis = 1, inplace = True)
+    return pd.DataFrame(articles), pd.DataFrame(all_stocks).T
 
-connection = connect_to_server("localhost", "root", "Password")
-if connection != None:
-    db_name = "Financial_Information"
-    cursor = connection.cursor()
+def save_to_csv(df, path):
+    header = not os.path.exists(path)
+    df.to_csv(path, mode='a', header=header, index=False)
+
+def save_to_mysql(df, db_name, table_name, host="localhost", user="root", password="Password"):
+    conn = connect_to_server(host, user, password)
+    if not conn:
+        return
+
+    cursor = conn.cursor()
     cursor.execute(f"CREATE DATABASE IF NOT EXISTS {db_name}")
+    conn.close()
 
-    cursor.close()
-    connection.close()
-    database_connection = connect_database("localhost", "root", "Password", db_name)
-    if database_connection is not None:
-        database_cursor = database_connection.cursor()
-        table_name = "Newspapers"
-        #data_tuples = [tuple(row) for row in df.itertuples(index = False)]
-        if not table_exists(database_cursor, table_name):
-            table_query ="""
-            CREATE TABLE Newspapers(
-                id INT AUTO_INCREMENT PRIMARY KEY,
-                link VARCHAR(255),
-                author VARCHAR(255),
-                title VARCHAR(255),
-                time DATETIME, 
-                stocks VARCHAR(255))"""
-            
-            
-            database_cursor.execute(table_query)
-            database_connection.commit()
-        
-        data_tuples = list(df.itertuples(index=False, name=None))
-        database_cursor.executemany("""
-        INSERT INTO Articles (link, author, title, time, stocks) VALUES (%s, %s, %s, %s, %s)
-        , data_tuples""")
-        database_connection.commit()
-        database_cursor.close()
-        database_connection.close()
+    db_conn = connect_database(host, user, password, db_name)
+    if not db_conn:
+        return
+
+    db_cursor = db_conn.cursor()
+    if not table_exists(db_cursor, table_name):
+        db_cursor.execute(f"""
+        CREATE TABLE {table_name} (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            link TEXT,
+            author VARCHAR(255),
+            title TEXT,
+            time DATETIME,
+            stocks TEXT
+        )""")
+
+    df['time'] = pd.to_datetime(df['time'], format='%a, %b %d, %Y, %I:%M %p', errors='coerce')
+    df = df.drop(columns=['sum_body'], errors='ignore')
+    tuples = list(df.itertuples(index=False, name=None))
+
+    db_cursor.executemany(
+        f"INSERT INTO {table_name} (link, author, title, time, stocks) VALUES (%s, %s, %s, %s, %s)",
+        tuples
+    )
+    db_conn.commit()
+    db_cursor.close()
+    db_conn.close()
+
+if __name__ == "__main__":
+    article_df, stock_df = scrape_yahoo_articles(limit=3)
+
+    save_to_csv(stock_df.replace('--', np.nan), r'C:\Users\thynnea\Downloads\stocks.csv')
+    save_to_csv(article_df, r'C:\Users\thynnea\Downloads\articles.csv')
+    save_to_mysql(article_df, db_name="Financial_Information", table_name="Articles")
